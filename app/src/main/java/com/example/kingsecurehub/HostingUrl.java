024a28e4379f0d2e505c7e7282f4dc8dd27fd98e
dani10mv <danielmirallesvargas@gmail.com>
2021-05-30 22:02:32 +0200

    update

--- app/src/main/java/com/example/kingsecurehub/HostingUrl.java
+++ app/src/main/java/com/example/kingsecurehub/HostingUrl.java
@@ -2,7 +2,7 @@ package com.example.kingsecurehub;
 
 public class HostingUrl {
     //https://kingserve.herokuapp.com     https://secretive-experienced-silene.glitch.me
-    private static String url= "https://kingserve.herokuapp.com  ";
+    private static String url= "https://kingserve.herokuapp.com";
 
     public static String getUrl(){
         return url;
